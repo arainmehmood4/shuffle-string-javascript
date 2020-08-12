574482dd7a3abc164440c20eaab5645a7728b6b3
arainmehmood4 <69026330+arainmehmood4@users.noreply.github.com>
2020-08-11 21:57:27 -0700

    Update shuffle_string.js

--- shuffle_string.js
+++ shuffle_string.js
@@ -1,5 +1,6 @@
 var swape=function(s,indices)
 {
+  //convert string to array
   var str=s.split("");
   for(var i=0; i<indices.length; i++)
       {
@@ -7,17 +8,19 @@ var swape=function(s,indices)
               {
                   if(indices[i] > indices[j])
                       {
+                        //swape indices
                           var temp=indices[j];
-                          var tem_s=str[j];
-                          
                           indices[j]=indices[i];
-                          str[j]=str[i]
-                          
-                          indices[i]=temp;
+                           indices[i]=temp;
+                        
+                        //swape string
+                        var tem_s=str[j];  
+                          str[j]=str[i];
                           str[i]=tem_s;
                       }
               }
       }
+  //return string after convert array to string
     return str.join("");
 }
 console.log(swape("codeleet",[4,5,6,7,0,2,1,3]));
